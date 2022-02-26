54f816943b72bfcbf06a9f9191d02754677da764
jitikajoshi <88429268+jitikajoshi@users.noreply.github.com>
2022-02-26 23:16:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import UsersCard from "./Components/users";
+import './App.css';
+import React, { Component } from 'react';
+	
+class App extends Component {
+  constructor(props){
+	super(props)
+		
+	// Set initial state
+	this.state = {users_data :[],
+                loading : true                
+  }
+  
+
+	this.updateState = this.updateState.bind(this)
+  }
+
+    
+  updateState(){
+      const link="https://reqres.in/api/users?page=1";
+      fetch(link)
+      .then(response => response.json())
+      .then((users) => {
+        
+        this.setState({users_data :users.data,
+                        loading: false
+        })
+        console.log(users.data);
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+    
+  render(){
+    return (<>
+      <nav>
+          <div className="box">
+            <div className="row">
+            <div className="column1">
+            <h2>User Profile App</h2>
+            </div>
+            <div className="column2">
+            <button onClick={this.updateState}>Get Users</button>
+            </div>
+            </div>
+          </div>
+        </nav>
+        <div className="box2">
+         <UsersCard loading={this.state.loading} users={this.state.users_data}/>
+         </div>
+
+         <div class="container my-50"> 
+         <br></br>
+         <footer className="footer">
+           Created By &copy; Jitika Joshi
+         </footer>
+         <footer className="footer">
+           LetsGrowMore - WebDevelopment Intern
+         </footer>
+         <br></br>
+         </div> 
+    </>
+    )
+  }
+}
+	
+export default App;
+
